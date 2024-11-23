0f846bd711cb731555a0fa12ab936ce6bbb76995
xiaoxin08120000 <167305771+xiaoxin08120000@users.noreply.github.com>
2024-11-23 14:10:07 +0800

    Update cmd_geoip_export.go
    Signed-off-by: xiaoxin08120000 <167305771+xiaoxin08120000@users.noreply.github.com>

--- cmd/sing-box/cmd_geoip_export.go
+++ cmd/sing-box/cmd_geoip_export.go
@@ -6,11 +6,11 @@ import (
 	"os"
 	"strings"
 
-	C "github.com/sagernet/sing-box/constant"
-	"github.com/sagernet/sing-box/log"
-	"github.com/sagernet/sing-box/option"
-	E "github.com/sagernet/sing/common/exceptions"
-	"github.com/sagernet/sing/common/json"
+	C "github.com/xiaoxin08120000/sing-box/constant"
+	"github.com/xiaoxin08120000/sing-box/log"
+	"github.com/xiaoxin08120000/sing-box/option"
+	E "github.com/xiaoxin08120000/sing/common/exceptions"
+	"github.com/xiaoxin08120000/sing/common/json"
 
 	"github.com/oschwald/maxminddb-golang"
 	"github.com/spf13/cobra"
